7970936b51054508101c1115723b2054e5912074
Ariel Robotti <arielrobotti@gmail.com>
2023-03-31 20:48:24 -0300

    FopundMe

---
+++ contracts/FoundMe.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract FoundMe {
+
+    mapping(address => uint256) public aportes;
+    function found() public payable{
+        aportes[msg.sender] += msg.value;
+    }
+    function getFoundFrom(address _founder)public view returns(uint256){
+        return aportes[_founder];
+    }
+    function getFoundByMe()public view returns(uint256){
+        return aportes[msg.sender];
+    }
+}
