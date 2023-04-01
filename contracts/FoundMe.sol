178ad21f8242b3351aaa05cdf3d8bf867f4988ce
ArielRobotti <arielrobotti@gmail.com>
2023-04-01 01:00:14 -0300

    tip & withdraw

--- contracts/FoundMe.sol
+++ contracts/FoundMe.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.2 <0.9.0;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-
+//import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FoundMe{
     mapping (address => uint256) addressToAmountFounded;
@@ -14,6 +13,16 @@ contract FoundMe{
     }
     function getMyAmount()public view returns(uint256){
         return addressToAmountFounded[msg.sender];
+    }
+    function sendTip(address payable _recipt)public payable{
+        uint256 tip = msg.value;
+        _recipt.transfer(tip-tip/10);  //el contrato se queda con el 10% de la propina
+    }
+    function withdraw(uint256 _amount)public payable{
+        require(addressToAmountFounded[msg.sender] >= _amount);
+        address payable recipient = payable(msg.sender);
+        addressToAmountFounded[msg.sender] -= _amount;
+        recipient.transfer(_amount);
     }
 }
 
