563672e674fce1a1d4ada9db9ddea54671dffee9
Ariel Robotti <arielrobotti@gmail.com>
2023-03-31 21:59:28 -0300

    22:00

--- contracts/FoundMe.sol
+++ contracts/FoundMe.sol
@@ -4,14 +4,16 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract FoundMe {
 
-    mapping(address => uint256) public aportes;
+    mapping(address => uint256) aportes;
+
     function found() public payable{
         aportes[msg.sender] += msg.value;
     }
     function getFoundFrom(address _founder)public view returns(uint256){
         return aportes[_founder];
     }
-    function getFoundByMe()public view returns(uint256){
+    function getFoundFromyMe()public view returns(uint256){
         return aportes[msg.sender];
     }
+    
 }
